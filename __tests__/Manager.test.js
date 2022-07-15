d7586d6e25d70f0f35bd923c8661c3b37e116fb5
Phil Branch <philbranch243@gmail.com>
2022-07-14 20:36:20 -0500

    copied method tests into manager file and adjusted.  They all are passing

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -9,3 +9,28 @@ test('check that manager object was created', ()=> {
     expect(manager.officeNumber).toStrictEqual(expect.any(Number));
 
 });
+
+
+test('check that we get manager name', () =>{
+    const manager = new Manager('Tim', 2, 'time@email.com', 12);
+
+    expect(manager.getName()).toEqual(expect.stringContaining(manager.name.toString()));
+});
+
+test('check that we get an manager id', () =>{
+    const manager = new Manager('Tim', 2, 'time@email.com', 12);
+
+    expect(manager.getId()).toEqual(expect.stringContaining(manager.id.toString()));
+});
+
+test('check that we get an email', () =>{
+    const manager = new Manager('Tim', 2, 'time@email.com', 12);
+
+    expect(manager.getEmail()).toEqual(expect.stringContaining(manager.email.toString()));
+})
+
+test('check that we get manager role', () =>{
+    const manager = new Manager('Tim', 2, 'time@email.com', 12);
+
+    expect(manager.getRole()).toBe('Manager');
+});
