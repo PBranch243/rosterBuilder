276b787f5d6f7f6ea2978a3f696c4bece273509d
Phil Branch <philbranch243@gmail.com>
2022-07-14 21:18:27 -0500

    finished intern class, all tests passing

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -9,3 +9,28 @@ test('check that intern object was created with the correct properties', () =>{
     expect(intern.school).toStrictEqual(expect.any(String));
 
 });
+
+
+test('check that we get employee name', () =>{
+    const intern = new Intern("Kim Tern", 7, "kim@email.com", "Some University");
+
+    expect(intern.getName()).toEqual(expect.stringContaining(intern.name.toString()));
+});
+
+test('check that we get an employee id', () =>{
+    const intern = new Intern("Kim Tern", 7, "kim@email.com", "Some University");
+
+    expect(intern.getId()).toEqual(expect.stringContaining(intern.id.toString()));
+});
+
+test('check that we get an email', () =>{
+    const intern = new Intern("Kim Tern", 7, "kim@email.com", "Some University");
+
+    expect(intern.getEmail()).toEqual(expect.stringContaining(intern.email.toString()));
+})
+
+test('check that we get employee role', () =>{
+    const intern = new Intern("Kim Tern", 7, "kim@email.com", "Some University");
+
+    expect(intern.getRole()).toBe('Intern');
+});
